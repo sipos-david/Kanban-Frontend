33a761a9251ee407f864bd0f00dd70be95ad8b16
Sipos Aron David <sipisipos@gmail.com>
2021-05-04 22:40:09 +0200

    Task: missing dialogs added

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -2,11 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Task } from 'src/app/shared/models/task.model';
+import { User } from 'src/app/shared/models/user.model';
 import { UserService } from 'src/app/shared/services/user.service';
 import { AddUserDialogComponent } from '../dialogs/add-user-dialog/add-user-dialog.component';
 import { AddUserDialogData } from '../dialogs/add-user-dialog/add-user-dialog.model';
 import { SimpleAddDialogData } from '../dialogs/simple-add-dialog/simple-add-dialog-data.model';
 import { SimpleAddDialogComponent } from '../dialogs/simple-add-dialog/simple-add-dialog.component';
+import { SimpleDialogComponent } from '../dialogs/simple-dialog/simple-dialog.component';
+import { SimpleDialogData } from '../dialogs/simple-dialog/simple-dialog.model';
 
 @Component({
   selector: 'app-task',
@@ -29,7 +32,7 @@ export class TaskComponent implements OnInit {
     const data = new SimpleAddDialogData();
     data.title = 'Add comment';
     data.subtitle = 'Please enter the comment: ';
-    data.placeholder = '...';
+    data.placeholder = 'Write something here...';
     const dialogRef = this.dialog.open(SimpleAddDialogComponent, {
       data,
     });
@@ -53,9 +56,25 @@ export class TaskComponent implements OnInit {
     });
   }
 
-  onAddUser(): void {
-    const data = new AddUserDialogData();
+  public onRemoveUser(user: User) {
+    const data = new SimpleDialogData();
+    data.title = 'Delete user';
+    data.subtitle =
+      'Are you sure you want to remove "' + user?.name + '" from this task?';
+    const dialogRef = this.dialog.open(SimpleDialogComponent, {
+      data,
+    });
 
+    dialogRef.afterClosed().subscribe((result: SimpleDialogData) => {
+      if (result) {
+        console.log('delete user');
+        // TODO: user delete from task
+      }
+    });
+  }
+
+  public onAddUser(): void {
+    const data = new AddUserDialogData();
     this.userService.getUsers().subscribe((users) => {
       data.title = "Add user's to task:";
       users.forEach((u) => {
