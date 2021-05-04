4dad1957a8951afd08a0d3e7fbcd73a0091c47a6
Sipos Aron David <sipisipos@gmail.com>
2021-05-04 22:38:42 +0200

    Comment: edit and remove dialogs

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Comment } from 'src/app/shared/models/comment.model';
+import { SimpleAddDialogData } from '../dialogs/simple-add-dialog/simple-add-dialog-data.model';
+import { SimpleAddDialogComponent } from '../dialogs/simple-add-dialog/simple-add-dialog.component';
+import { SimpleDialogComponent } from '../dialogs/simple-dialog/simple-dialog.component';
+import { SimpleDialogData } from '../dialogs/simple-dialog/simple-dialog.model';
 
 @Component({
   selector: 'app-comment',
@@ -7,13 +12,43 @@ import { Comment } from 'src/app/shared/models/comment.model';
   styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   @Input() public data: Comment | undefined;
 
   ngOnInit(): void {}
 
-  public onDeleteComment(): void {}
+  public onDeleteComment(): void {
+    const data = new SimpleDialogData();
+    data.title = 'Delete comment';
+    data.subtitle = 'Are you sure you want to remove this comment?';
+    const dialogRef = this.dialog.open(SimpleDialogComponent, {
+      data,
+    });
 
-  public onEditComment(): void {}
+    dialogRef.afterClosed().subscribe((result: SimpleDialogData) => {
+      if (result) {
+        console.log('delete comment');
+        // TODO: comment delete from task
+      }
+    });
+  }
+
+  public onEditComment(): void {
+    const data = new SimpleAddDialogData();
+    data.title = 'Edit comment';
+    data.subtitle = 'Please edit the comment:';
+    data.placeholder = 'New text...';
+    data.text = this.data?.text;
+    const dialogRef = this.dialog.open(SimpleAddDialogComponent, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe((result: SimpleAddDialogData) => {
+      if (result && result.text && result.text !== '' && this.data != null) {
+        this.data.text = result.text;
+        // TODO: edit comment on server
+      }
+    });
+  }
 }
