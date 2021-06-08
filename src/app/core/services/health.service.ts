d3d650ee66be526d239281c5faceb6979b1127e2
Sipos Aron David <sipisipos@gmail.com>
2021-06-08 12:28:06 +0200

    Added health logging

--- src/app/core/services/health.service.ts
+++ src/app/core/services/health.service.ts
@@ -1,17 +1,28 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HealthService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private loggingService: LoggingService
+  ) {}
 
   getApiHealth(): Observable<string> {
-    return this.http.get(environment.server.health, {
-      responseType: 'text',
-    });
+    return this.http
+      .get(environment.server.health, {
+        responseType: 'text',
+      })
+      .pipe(
+        tap((response) => {
+          this.loggingService.log('Server health check : ' + response);
+        })
+      );
   }
 }
